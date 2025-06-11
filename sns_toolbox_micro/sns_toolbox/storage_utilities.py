0046d119fe34b6107765bdfd9943299fdb497e08
trystonfreeman <trystonfreeman@gmail.com>
2025-06-11 14:16:35 -0400

    Added library required in micropython environment
    - Intended for precompiled networks, having no functionality to build networks
    - Implements storage_utilities.py and backends.py into micropython
    - Only implements numpy backend due to hardware limitations

---
+++ sns_toolbox_micro/sns_toolbox/storage_utilities.py
@@ -0,0 +1,42 @@
+"""
+Utility functions for saving and loading compiled SNS networks.
+"""
+from sns_toolbox.backends import Backend,SNS_Numpy
+import json
+
+# Allows Storage Utilities to work on micropython installations with ulab
+try:
+    from ulab import numpy as np
+    micropython = True
+except ImportError:
+    import numpy as np
+    import torch
+    import pickle
+    micropython = False
+def save(model: Backend, filename: str,params: dict = None) -> None:
+
+    # Required if using model for micropython
+    if params != None:
+        print("Saved model will only work on micro-SNS!")
+        # Converts ndarrays to lists that JSON module can handle
+        for x,y in params:
+            if isinstance(y,torch.Tensor):
+                print('Pytorch not compatable with micro-SNS!')
+                return
+            if isinstance(y,np.ndarray):
+                params[x] = y.tolist()
+        json.dump(params, open(filename, 'wb'))
+    else:
+        pickle.dump(model, open(filename,'wb'))
+
+def load(filename) -> Backend:
+    if not micropython:
+        model = pickle.load(open(filename, 'rb'))
+    else:
+        f = open(filename)
+        params = json.loads(f.read())
+        for x,y in params.items():
+            if type(y)==list:
+                params[x] = np.array(y)
+        model = SNS_Numpy(params)
+    return model
